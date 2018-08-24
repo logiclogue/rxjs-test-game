7253e764d0afb7f2b6e9b84680aa61085a089e9c
logiclogue <jordanpaullord@hotmail.co.uk>
2018-08-24 22:49:22 +0100

    Adds very basic hello I'm here

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: "./index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    devtool: "source-maps",
+    entry: {
+        main: "./src/index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
+};
