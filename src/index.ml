74f57239577b5979b75319e99e7f329399d335ac
logiclogue <jordanpaullord@hotmail.co.uk>
2018-09-16 22:13:13 +0100

    Adds the tick stream to the game state modifiers

--- src/index.ml
+++ src/index.ml
@@ -4,9 +4,15 @@ let loop_stream = RxJS.interval 0 RxJS.animation_frame
 let player_modifier_stream = Keydown.up_stream
     |> RxJS.map_to (GameState.update_player Player.go_north)
 
+let tick_modifier_stream = RxJS.set_interval 100
+    |> RxJS.map_to GameState.tick
+
 let reduce acc value _index = value acc
 
-let game_state_stream = RxJS.merge [|player_modifier_stream|]
+let game_state_stream = RxJS.merge [|
+    player_modifier_stream;
+    tick_modifier_stream
+|]
     |> RxJS.scan reduce GameState.default
 
 let draw = RxJS.with_latest_from game_state_stream loop_stream
