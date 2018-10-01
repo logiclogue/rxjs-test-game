1b38c1d7746852afa66999167cca5f4169e4257f
logiclogue <jordanpaullord@hotmail.co.uk>
2018-10-01 22:17:23 +0100

    Updating keydown to include a map for all keys that are being pressed, failing

--- src/Keydown.ml
+++ src/Keydown.ml
@@ -1,19 +1,23 @@
-type keydown
+type key
 
 let document = [%raw {| document |}]
 
-external get_key_code : keydown -> int = "keyCode" [@@bs.get]
+external get_key_code : key -> int = "keyCode" [@@bs.get]
 
-let keydown_stream
-: keydown RxJS.observable
-= RxJS.from_event document "keydown"
-    |> RxJS.share ()
+let event_stream event_name
+    = RxJS.from_event document event_name |> RxJS.share ()
 
-let key_code_stream = RxJS.map get_key_code keydown_stream
+let keydown_stream : key RxJS.observable = event_stream "keydown"
+let keyup_stream   : key RxJS.observable = event_stream "keyup"
 
-let filter_key_stream x = RxJS.filter ((==) x) key_code_stream
+let keydown_code_stream = RxJS.map get_key_code keydown_stream
+let keyup_code_stream   = RxJS.map get_key_code keyup_stream
+
+let filter_key_stream x = RxJS.filter ((==) x) keydown_code_stream
 
 let left_stream  = filter_key_stream 37
 let up_stream    = filter_key_stream 38
 let right_stream = filter_key_stream 39
 let down_stream  = filter_key_stream 40
+
+let keydown_modifier = keydown_code_stream |> RxJS.map List.add
